90f94c1b4661e8ff76b8702193499527f3420d69
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2022-01-19 21:26:58 +0700

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
