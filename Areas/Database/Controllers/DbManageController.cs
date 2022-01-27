afbab9dd1811a2b03ea4c0c517ee6daa23c5ab60
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2022-01-27 16:30:51 +0700

    Delete Database

--- Areas/Database/Controllers/DbManageController.cs
+++ Areas/Database/Controllers/DbManageController.cs
@@ -2,17 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AppMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppMVC.Areas.Database.Controllers
 {
+    [Area("Database")]
+    [Route("/database-manage/[action]")]
     public class DbManageController : Controller
     {
-        [Area("Database")]
-        [Route("/database-manage/[action]")]
+        private readonly AppDbContext _dbContext;
+
+        public DbManageController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteDb()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteDbAsync()
+        {
+            var success = await _dbContext.Database.EnsureDeletedAsync();
+
+            StatusMessage = success ? "Delete database succeed" : "Delete database failed";
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
