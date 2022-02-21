65415255431f72da34a1625270046beb10620c25
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2022-02-21 22:53:11 +0700

    Fake Categories and Posts

--- Areas/Database/Controllers/DbManageController.cs
+++ Areas/Database/Controllers/DbManageController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppMVC.Areas.Identity.Data;
 using AppMVC.Models;
+using AppMVC.Models.Blog;
+using Bogus;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,10 +19,12 @@ namespace AppMVC.Areas.Database.Controllers
     public class DbManageController : Controller
     {
         private readonly AppDbContext _dbContext;
+        private readonly UserManager<AppUser> _userManager;
 
-        public DbManageController(AppDbContext dbContext)
+        public DbManageController(AppDbContext dbContext, UserManager<AppUser> userManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
         }
 
         [TempData]
@@ -54,6 +59,77 @@ namespace AppMVC.Areas.Database.Controllers
             StatusMessage = "Update database succeed";
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> SeedDataAsync()
+        {
+            await SeedPostCategory();
+
+            StatusMessage = "Seeded Database";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task SeedPostCategory()
+        {
+            // Remove fake categories and posts before fake
+            _dbContext.Categories.RemoveRange(_dbContext.Categories.Where(c => c.Description.Contains("[fakeData]")));
+            _dbContext.Posts.RemoveRange(_dbContext.Posts.Where(p => p.Content.Contains("[fakeData]")));
+
+            // Fake categories
+            var fakerCategory = new Faker<Category>();
+            int numCat = 1;
+            fakerCategory.RuleFor(c => c.Title, f => $"Category {numCat++} " + f.Lorem.Sentence(1, 2).Trim('.'));
+            fakerCategory.RuleFor(c => c.Description, f => f.Lorem.Sentences(5) + "[fakeData]");
+            fakerCategory.RuleFor(c => c.Slug, f => f.Lorem.Slug());
+
+            var cat1 = fakerCategory.Generate();
+            var cat11 = fakerCategory.Generate();
+            var cat12 = fakerCategory.Generate();
+            var cat2 = fakerCategory.Generate();
+            var cat21 = fakerCategory.Generate();
+            var cat211 = fakerCategory.Generate();
+
+            cat11.ParentCategory = cat1;
+            cat12.ParentCategory = cat1;
+            cat21.ParentCategory = cat2;
+            cat211.ParentCategory = cat21;
+
+            var categories = new Category[] { cat1, cat11, cat12, cat2, cat21, cat211 };
+            await _dbContext.Categories.AddRangeAsync(categories);
+
+            // Fake posts
+            var randomCatIndex = new Random();
+            int numPost = 1;
+            var user = _userManager.GetUserAsync(this.User).Result;
+
+            var fakerPost = new Faker<Post>();
+            fakerPost.RuleFor(p => p.AuthorId, f => user.Id);
+            fakerPost.RuleFor(p => p.Content, f => f.Lorem.Paragraphs(7) + "[fakeData]");
+            fakerPost.RuleFor(p => p.DateCreated, f => f.Date.Between(new DateTime(2021, 1, 1), new DateTime(2022, 2, 1)));
+            fakerPost.RuleFor(p => p.Published, f => true);
+            fakerPost.RuleFor(p => p.Slug, f => f.Lorem.Slug());
+            fakerPost.RuleFor(p => p.Title, f => $"Post {numPost++} " + f.Lorem.Sentence(3, 4).Trim('.'));
+
+            List<Post> posts = new List<Post>();
+            List<PostCategory> postCategories = new List<PostCategory>();
+
+            for (int i = 0; i < 40; i++)
+            {
+                var post = fakerPost.Generate();
+                post.DateUpdated = post.DateCreated;
+                posts.Add(post);
+
+                postCategories.Add(new PostCategory()
+                {
+                    Post = post,
+                    Category = categories[randomCatIndex.Next(6)]
+                });
+            }
+            await _dbContext.Posts.AddRangeAsync(posts);
+            await _dbContext.PostCategories.AddRangeAsync(postCategories);
+
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
